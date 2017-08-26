81c603b044a620b205ee179960daa4d737ca19ec
PeterPan718 <31224250+PeterPan718@users.noreply.github.com>
2017-08-26 16:11:51 -0600

    commit solution

---
+++ PeopleSearch/Migrations/Configuration.cs
@@ -0,0 +1,61 @@
+namespace PeopleSearch.Migrations
+{
+	using PeopleSearch.Models;
+	using System;
+	using System.Data.Entity;
+	using System.Data.Entity.Migrations;
+	using System.Linq;
+
+	internal sealed class Configuration : DbMigrationsConfiguration<PeopleSearch.Models.PeopleDBContext>
+	{
+		public Configuration()
+		{
+			AutomaticMigrationsEnabled = false;
+		}
+
+		protected override void Seed(PeopleSearch.Models.PeopleDBContext context)
+		{
+			//  This method will be called after migrating to the latest version.
+
+			context.People.AddOrUpdate(i => i.PersonID,
+				new Person
+				{
+					PersonID = 1,
+					FirstName = "Alex",
+					LastName = "Luka",
+					Address = "Berlin",
+					Age = 16,
+					Interests = "None",
+					Picture = "~/Photos/JohnSmith.png",
+					AlternateText = "John Smith Photo"
+				},
+
+				new Person
+				{
+					PersonID = 2,
+					FirstName = "Bob",
+					LastName = "Nelson",
+					Address = "Texas",
+					Age = 41,
+					Interests = "Paragling",
+					Picture = "~/Photos/BobNelson.png",
+					AlternateText = "Bob Nelson Photo"
+				},
+
+				new Person
+				{
+					PersonID = 3,
+					FirstName = "Cindy",
+					LastName = "Crawford",
+					Address = "Rio",
+					Age = 86,
+					Interests = "Drawing",
+					Picture = "~/Photos/CindyCrawford.png",
+					AlternateText = "Cindy Crawford Photo"
+				}
+			);
+		}
+
+
+	}
+}
